49bc68d04174501b1e897fb2e4c8d927fa21dc82
Omar Salum <o.e.salum@gmail.com>
2023-03-02 21:40:42 -0300

    agregando spinner

--- EnviarEmail/js/app.js
+++ EnviarEmail/js/app.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');  
 
     // Asignar eventos
 
@@ -20,17 +21,43 @@ document.addEventListener('DOMContentLoaded', function() {
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
 
+    formulario.addEventListener('submit', enviarEmail);
+
     btnReset.addEventListener('click', e => {
         // e.preventDefault();
         
-        // Reiniciar el objeto. Si no lo hacemos, el objeto queda cargado y el botón de enviar queda habilitado (ver método comprobarEmail())
-        email.email = '';
-        email.asunto = '';
-        email.mensaje = '';
-
-        comprobarEmail();
+        resetFormulario();
 
     });
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 
+            'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente.';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+
+                alertaExito.remove();
+
+            }, 3000)
+
+        }, 3000);
+    }
 
     function validar(e) {
         //console.log(e.target.parentElement); // Del elemento que estoy apuntando, me voy a su elemento padre
@@ -95,6 +122,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false;
+    }
+
+    function resetFormulario() {
+        // Reiniciar el objeto. Si no lo hacemos, el objeto queda cargado y el botón de enviar queda habilitado (ver método comprobarEmail())
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
     }
  
 });
